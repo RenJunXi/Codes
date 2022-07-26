4d3a9945c599262bf932c3e67ae48d62cc9ae206
alan <769825994@qq.com>
2022-07-26 14:47:13 +0800

    commit

--- CC200000[编程乐园] 树的遍历.cpp
+++ CC200000[编程乐园] 树的遍历.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+vector<int> trees[101];
 int n;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        vector[a].push_back(b);
+    }
 }
