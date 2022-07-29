b35a983a5958c734e25b37adc6bdd8693be9058c
alan <769825994@qq.com>
2022-07-29 21:55:20 +0800

    Commit

--- HT110121[哈工科教] 众数/main.cpp
+++ HT110121[哈工科教] 众数/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
+#define max __INSTATED_MAX
 using namespace std;
-int main()
-{
+int N,num = 0,max = 0,x = 0,a[30001];
+int main(){
     ios::sync_with_stdio(false);
-    int N;
-    while(cin>>N&&N!=0){
-        int *a=new int[N];
-        for(int i=0;i<N;i++){
-            cin>>a[i];
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    for (int i = 0;i < N;i++)
+        cin >> a[i];
+    num = 0;
+    max = 0;
+    x = 0;
+    for (int i = 0;i <= N;i++){
+        if (a[i - 1] == a[i]){
+            num++;
         }
-        int num=0,max=0,x=0;
-        for(int i=0;i<=N;i++){
-            if(a[i-1]==a[i]){
-                num++;
+        else{
+            if (max < num){
+                max = num;
+                x = a[i - 1];
             }
-            else{
-                if(max<num){
-                    max=num;
-                    x=a[i-1];
-                }
-                num=0;
-            }
+            num = 0;
         }
-        cout<<x<<endl;
     }
+    cout << x << endl;
     return 0;
 }
