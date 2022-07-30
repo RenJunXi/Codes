036bde772e18d2501ef29ea103e30d90eb615695
alan <769825994@qq.com>
2022-07-30 14:58:49 +0800

    Commit

--- P2752 [USACO4.3]街道赛跑Street Race /main.cpp
+++ P2752 [USACO4.3]街道赛跑Street Race /main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+vector<int> graph[101];
+bool have_visited[101];
+int id;
+void run(int i){
+    have_visited[i] = true;
+    for (int j = 1;j <= graph[i].size();j++)
+        if (graph[i][j] == 1 and !have_visited[j])
+            run(i);
+}
+void must_through(){
+    ;
+}
+void input(){
+    for (int k = 0 ;k != -1;cin >> k){
+        while (k >= 0){
+            graph[id].push_back(k);
+            cin >> k;
+        }
+        if (k == -2)
+            id++;
+    }
+}
 int main(){
-    std::cout << "Hello, World!" << std::endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    input();
+    must_through();
     return 0;
 }
